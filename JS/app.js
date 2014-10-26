ebc99e9f1707b7c89c1fef5b24bcfc424e74de18
avinnakota <avinnakotaster@gmail.com>
2014-10-26 12:11:53 -0700

    Latest
    Can’t get checkmark to add to list

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+	//The following code adds items to the list
 	$("#add").on("click", function(){
 		var listItem = $("#item").val();
-		$(this).closest("#note").find("ul").append("<li>" + listItem + "</li>");
+		$(this).closest("#note").find("ul").append("<li>" + listItem  + "</li>");
 	});
+	//This code makes the list items sortable
+	$("ul").sortable({ axis: "y" });
 });
