87e463289096768a4e9d778ffca6e3b75858797b
avinnakota <avinnakotaster@gmail.com>
2014-10-26 19:57:28 -0700

    Can't append new button

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
 	//The following code adds items to the list
 	$("#add").on("click", function(){
 		var listItem = $("#item").val();
-		$(this).closest("#note").find("ul").append("<li>" + listItem  + "</li>");
+		$(this).closest("#note").find("ul").append
+		("<li><button class = "rid">-</button><p class = "itembar">"	
+		+ listItem  + "</p></li>");
+	});
+	//This code deletes 
+	$("ul").on("click", "li", function(){
+		(this).remove();
 	});
 	//This code makes the list items sortable
 	$("ul").sortable({ axis: "y" });
